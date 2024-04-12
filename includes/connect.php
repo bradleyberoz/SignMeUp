ef045b7763708a59a1c2e14fe7f92c154e825d98
bradleyberoz <96027996+bradleyberoz@users.noreply.github.com>
2024-04-12 16:02:06 -0400

    Create connect.php

---
+++ includes/connect.php
@@ -0,0 +1,5 @@
+<?php
+	$smeConn=new mysqli("localhost", "mickeye1_signmeup", "sign.me.up", "mickeye1_signmeup");
+if(mysqli_connect_errno())
+    echo "Connection failed: ".mysqli_connect_error();
+?>
